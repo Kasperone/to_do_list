25255c8c3dcfc54e0810084e7fd575f868baabc0
Kasperone <dwaka87@o2.pl>
2022-03-26 12:08:07 +0100

    added_all_basic_functionality

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# to_do_list
+# TODO list
+
+Just a simple Todo list.
+
+## Description
+
+Todo list I made during the Udemy course by Mateusz Maj.
+
+* link to the course [Mateusz Maj Course](https://www.udemy.com/course/javascript-jedyny-kurs-ktorego-potrzebujesz/) (the course is in Polish)
+
+## Getting Started
+### Dependencies
+
+* You don't need any dependencies
+
+### Installing
+
+* Just fork, clone or download the project.
+
+### Executing program
+
+* Open index.html file in your browser.
+
+## Authors
+
+ex. Kasper Kosecki
+ex. Mateusz Maj
+
+## License
+
+* Beerware License.
+
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let todoInput, errorInfo, addBtn, ulList, newTodo
+let todoInput, errorInfo, addBtn, ulList,
+    newTodo, popup, popupInfo, todoToEdit,
+    popupInput, popupAddBtn, popupCloseBtn
 
 const main = () => {
     prepareDOMElements()
@@ -10,24 +12,104 @@ const prepareDOMElements = () => {
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
     ulList = document.querySelector('.todolist ul')
+
+    popup = document.querySelector('.popup')
+    popupInfo = document.querySelector('.popup-info')
+    popupInput = document.querySelector('.popup-input')
+    popupAddBtn = document.querySelector('.accept')
+    popupCloseBtn = document.querySelector('.cancel')
 }
 
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTodo)
+    ulList.addEventListener('click', checkClick)
+    popupCloseBtn.addEventListener('click', closePopup)
+    popupAddBtn.addEventListener('click', changeTodoText)
+    todoInput.addEventListener('keyup', enterKeyCheck)
 }
 
 const addNewTodo = () => {
     if(todoInput.value !== ""){
         newTodo = document.createElement('li')
         newTodo.textContent = todoInput.value
+        createToolsArea()
+
         ulList.append(newTodo)
 
         todoInput.value = ""
         errorInfo.textContent = ""
     } else {
-        errorInfo.textContent = "Wpisz treść zadania!"
+        errorInfo.textContent = "Enter the content of the task!"
     }
 }
 
+const createToolsArea = () => {
+
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    newTodo.append(toolsPanel)
+
+    const completeBtn = document.createElement('button')
+    completeBtn.classList.add('complete')
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.textContent = "EDIT"
+
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('delete')
+    delBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    toolsPanel.append(completeBtn, editBtn, delBtn)
+}
+
+const checkClick = e => {
+    if(e.target.matches('.complete')) {
+        e.target.closest('li').classList.toggle('completed')
+        e.target.classList.toggle('completed')
+    } else if (e.target.matches('.edit')) {
+            editTodo(e)
+    } else if (e.target.matches('.delete')) {
+            deleteTodo(e)
+    }
+}
+
+const editTodo = e => {
+    todoToEdit = e.target.closest('li')
+    popupInput.value = todoToEdit.firstChild.textContent
+    popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+    popup.style.display = 'none'
+    popupInfo.textContent = ""
+}
+
+const changeTodoText = () => {
+    if(popupInput.value !== "") {
+        todoToEdit.firstChild.textContent = popupInput.value
+        popup.style.display = 'none'
+        popupInfo.textContent = ""
+    } else {
+        popupInfo.textContent = "You need to enter some content!"
+    }
+}
+
+const deleteTodo = e => {
+    e.target.closest('li').remove()
+
+    const allTodos = ulList.querySelectorAll('li')
+
+    if(allTodos.length === 0) {
+        errorInfo.textContent = "No tasks on the list."
+    }
+}
+
+const enterKeyCheck = e => {
+    if(e.key === 'Enter') {
+        addNewTodo()
+    }
+}
 
 document.addEventListener('DOMContentLoaded', main)
